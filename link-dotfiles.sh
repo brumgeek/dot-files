215eb15587475d067093c0137d11cde5a6fc2b12
Douglas Allerton <drlinux@gmail.com>
2015-01-23 14:29:14 +0000

    Added link-dotfiles.sh to sym link dotfiles to home dir

---
+++ link-dotfiles.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Important this should be run from the directory that contains your dotfiles
+# This script safely makes symlinks with dotfiles to keep all of my devices
+# in sync. Before overwrriting any existing dotfiles with our new symlinked
+# dotfiles, we make sure to move existing dotfiles into ~/dofiles_old
+configfiles=".vimrc .gitconfig .zshrc .Xresources .aliases .tmux.clipboard .tmux.conf .zshrc.local"
+
+# Move any existing dotfiles in ~ to dotfiles_old directory and then create 
+# symlinks for all listed $configfiles from this dotfiles directory.
+dotfiles_old=~/dotfiles_old
+mkdir -p $dotfiles_old
+for file in $configfiles; do
+    if [ -e ~/$file ]; then
+        echo "Moving $file from ~/ to ~$dotfiles_old/"
+        mv ~/$file $dotfiles_old
+    fi
+    echo "Creating symlink to $file in home directory."
+    ln -s `pwd`/$file ~/$file
+done
+
